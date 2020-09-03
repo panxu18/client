db5222707331ab74ae245a720de6344e71e90712
panxu18 <xp545945@163.com>
2020-09-03 18:55:17 +0800

    add netty demo

---
+++ auth/src/main/java/channelstatus/HelloServerInitializer.java
@@ -0,0 +1,19 @@
+package channelstatus;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+/**
+ * channel 绑定时会进行handler的初始化，定义了一个http解码器，以及一个自定义的处理器
+ */
+public class HelloServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast("httpDecoderHandler", new HttpServerCodec());
+        pipeline.addLast("helloHttpHandler", new HelloHttpHandler());
+    }
+}
---
+++ auth/src/main/java/helloword/HelloServer.java
@@ -0,0 +1,38 @@
+package helloword;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+public class HelloServer {
+
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup boosGroup = new NioEventLoopGroup(); // accept
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(boosGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast("httpDecoderHandler", new HttpServerCodec());
+                            pipeline.addLast("helloHttpHandler", new HelloHttpHandler());
+                        }
+                    });
+            ChannelFuture channelFuture = serverBootstrap.bind(8080).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            boosGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
